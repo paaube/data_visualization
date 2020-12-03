1bb82482a1bce8b32597555f974d9076d3f77e9e
paaube <pierrealexaube@gmail.com>
2020-12-02 22:48:22 -0500

    Added D6 plotly.

--- die_visual.py
+++ die_visual.py
@@ -25,6 +25,6 @@ data = [Bar(x=x_val, y=frequencies)]
 x_axis_config = {'title': 'Result'}
 y_axis_config = {'title': 'Frequency of Result'}
 my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config, yaxis = y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='D6.html')
 
 
