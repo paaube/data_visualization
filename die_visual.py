d85e584f7feb72e4e1efc083ecad83e3a3f44efa
paaube <pierrealexaube@gmail.com>
2020-12-04 15:35:31 -0500

    Simulated dice rolls.

--- die_visual.py
+++ die_visual.py
@@ -24,6 +24,7 @@ data = [Bar(x=x_val, y=frequencies)]
 
 x_axis_config = {'title': 'Result'}
 y_axis_config = {'title': 'Frequency of Result'}
+
 my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config, yaxis = y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename='D6.html')
 
