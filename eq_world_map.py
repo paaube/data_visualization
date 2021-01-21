2460d2f996b08075615db6411b76739b4ee49e61
paaube <pierrealexaube@gmail.com>
2021-01-20 22:54:13 -0500

    Added hover text to map.

--- eq_world_map.py
+++ eq_world_map.py
@@ -4,7 +4,7 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # explore the structure of the data
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -14,20 +14,30 @@ with open(readable_file, 'w') as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 # map the earthquakes
 data = [{
     'type':'scattergeo',
     'lon':lons,
     'lat':lats,
+    'text': hover_texts,
+    'marker': {
+        'size':[5*mag for mag in mags],
+        'color':mags,
+        'colorscale':'Viridis',
+        'reversescale': True,
+        'colorbar': {'title':'Magnitude'}
+    },
 }]
 my_layout = Layout(title='Global Earthquakes')
 
