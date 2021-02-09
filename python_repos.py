d74224659c8547fc03a0a46c2d9eb0fd4c642518
paaube <pierrealexaube@gmail.com>
2021-02-08 23:05:24 -0500

    Started API visualization.

--- python_repos.py
+++ python_repos.py
@@ -1,29 +1,51 @@
 import requests
 
+from plotly.graph_objs import Bar
+from plotly import offline
+
 # make an API call and store the response
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept':'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
-# store API response variable
+# Process results
 response_dict = r.json()
-print(f"Total repositories: {response_dict['total_count']}")
-
-# explore info about the repositories
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
+repo_names, stars = [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
 
-# examine the first repository
-repo_dict = repo_dicts[0]
+# Make visualization
+data = [{
+    'type':'bar',
+    'x':repo_names,
+    'y':stars, 
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
+}]
 
-print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+my_layout = {
+    'title': 'Most-Starred Python Projects on GitHub',
+    'titlefont': {'size': 28},
+    'xaxis':{
+        'title':'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+        },
+    'yaxis':{
+        'title':'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+        },
+}
+
+fig = {'data':data, 'layout':my_layout}
+offline.plot(fig, filename='python_repos.html')
+
 
 
